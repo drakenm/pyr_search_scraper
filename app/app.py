0d6c0679ab9d96443a7d3d14b9ee35dca85ae093
drakenm <drakenm@gmail.com>
2024-03-19 00:00:59 -0500

    get price str, sms on new item found

--- app/app.py
+++ app/app.py
@@ -40,32 +40,99 @@ def matchFound( needle:re.Pattern, haystack:str ) -> bool:
         return False
     return True
 
-if r.status_code == 200:
-    lgr.debug( 'Request successful' )
-    page = soup( r.content, 'html.parser' )
-    search_result = page.find_all( attrs={"data-testid":"post-title-text"} )
-    lgr.debug( f'Found {len(search_result)} search results' )
+def send_sms( key:str, message:str ) -> str:
+    url = f"https://sandbox.dialpad.com/api/v2/sms?apikey={key}"
+    payload = {
+        'from_number': env['sms_from'],
+        'to_numbers': [{env['sms_to']}],
+        'text': message
+    }
+    headers = {
+        'content-type': 'application/json',
+        'accept': 'application/json'
+    }
+    try:
+        r = requests.post( url, json=payload, headers=headers )
+    except requests.exceptions.RequestException as e:
+        lgr.error( f'Failed to send SMS: {e}' )
+        return f'Failed to send SMS: {e}'
+    if r.status_code != 200:
+        lgr.error( f'Failed to send SMS: {r.status_code}' )
+        lgr.error( f'Response: {r.text}' )
+    return r.text
 
-    dbm = Database_Manager( sqlite3.connect( env['app_db'] ) )
+if r.status_code != 200:
+    lgr.error( f'Request failed with status code: {r.status_code}' )
+    sys.exit(1)
 
-    for index, item in enumerate(search_result):
-        if not item:
-            lgr.debug( 'No title found' )
-            continue
-        text = item.text.strip()
-        href = item['href']
-        filter_text_re = re.compile( r'' + env['filter_pattern'] )
-        search_text_re = re.compile( r'' + env['search_text'], re.IGNORECASE )
+lgr.debug( 'Request successful' )
+page = soup( r.content, 'html.parser' )
+search_result = page.find_all( attrs={"data-testid":"post-title-text"} )
+lgr.debug( f'Found {len(search_result)} search results' )
 
-        if not matchFound( filter_text_re, text ) or not matchFound( search_text_re, text ): continue
+dbm = Database_Manager( sqlite3.connect( env['app_db'] ) )
 
-        hash = get_hash( (text, href) )
-        # check if hash exists in db
-        if dbm.select_column( table='results', column='hash', data=hash ):
-            lgr.debug( f'{index}\tText: {text}' )
-            lgr.debug( f'>\t>\t> Already exists in db' )
-            continue
-        dbm.add_result( hash, text, href )
+aldi_finds:dict = {}
 
-        lgr.debug( f'{index}\tText: {text}' )
-        lgr.debug( f'\t\tHREF: {href}' )
+for index, item in enumerate(search_result):
+    if not item:
+        lgr.debug( 'No title found' )
+        continue
+    title = item.text.strip()
+    href = item['href']
+    filter_text_re = re.compile( r'' + env['filter_pattern'] )
+    search_text_re = re.compile( r'' + env['search_text'], re.IGNORECASE )
+
+    if not matchFound( filter_text_re, title ) or not matchFound( search_text_re, title ): continue
+
+    hash = get_hash( (title, href) )
+    # check if hash exists in db
+    if dbm.select_column( table='results', column='hash', data=hash ):
+        lgr.debug( f'{index}. Already exists in db...' )
+        lgr.debug( f'\t {hash}' )
+        lgr.debug( f'\t {title}' )
+        lgr.debug( f'\t {href}' )
+        continue
+
+    lgr.info(f'{index}. New result found!' )
+    lgr.info( f'\t {title}' )
+    lgr.info( f'\t {href}' )
+
+    result_url = f"{env['url_base']}{href}"
+
+
+    lgr.debug( f'\tSearch Result Item URL: {result_url}' )
+    result_get = requests.get( result_url, headers={'User-agent': 'Mozilla/5.0'} )
+    result_page = soup( result_get.content, 'html.parser' )
+    price_text_re = re.compile( r'\$\d{1,6}' )
+    result_content = result_page.find_all( string=price_text_re )
+    
+    if len(result_content) > 0:
+        lgr.info( f'Found {len(result_content)} results containing a dollar sign ($)...' )
+        for index, item in enumerate(result_content):
+            body_snippet = item.text.lower().strip()
+            find_text = env['search_text'].lower()
+            if len(result_content) == 1:
+                lgr.debug( "\tOnly one result found...")
+                lgr.debug( f'\tprice snippet is most likely:' )
+                lgr.debug( f'\t\t> {body_snippet}' )
+                price_snippet = body_snippet
+            elif index >= 1 and body_snippet.find(find_text) >= 0:
+                lgr.debug( f'\tprice snippet is most likely:' )
+                lgr.debug( f'\t\t> {body_snippet}' )
+                price_snippet = body_snippet
+
+    aldi_finds[hash] = {'title': title, 'url': result_url, 'price': price_snippet}
+    dbm.add_result( hash, title, result_url, price_snippet )
+
+for k, v in aldi_finds.items():
+    lgr.info( f'Found {v["title"]} at {v["url"]} for {v["price"]}' )
+    lgr.debug("building sms message...")
+    message = f"New Ad Posted for {env['search_text']}:\n\n{v['title']}\n{v['url']}\nPrice: {v['price']}"
+    lgr.debug( f"{message}" )
+    if env['sms_key']:
+        lgr.debug( f"Sending SMS..." )
+        send_sms( env['sms_key'], message )
+
+
+lgr.debug( '======App end logging======' )
