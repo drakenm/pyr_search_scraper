80a5340e54e7ef46b5ae08f6f1bd439088e60571
drakenm <drakenm@gmail.com>
2024-03-20 13:07:15 -0500

    fix price_snippet search

--- app/app.py
+++ app/app.py
@@ -95,6 +95,7 @@ for index, item in enumerate(search_result):
         continue
 
     lgr.info(f'{index}. New result found!' )
+    lgr.info( f'\t {hash}' )
     lgr.info( f'\t {title}' )
     lgr.info( f'\t {href}' )
 
@@ -110,18 +111,21 @@ for index, item in enumerate(search_result):
     if len(result_content) > 0:
         lgr.info( f'Found {len(result_content)} results containing a dollar sign ($)...' )
         for index, item in enumerate(result_content):
-            body_snippet = item.get_text(strip=True, separator=" ").lower().strip()
-            find_text = env['search_text'].lower()
+            body_snippet:str = item.get_text(strip=True, separator=" ").lower().strip()
             if len(result_content) == 1:
                 lgr.debug( "\tOnly one result found...")
                 lgr.debug( f'\tprice snippet is most likely:' )
                 lgr.debug( f'\t\t> {body_snippet}' )
                 price_snippet = body_snippet
-            elif index >= 1 and body_snippet.find(find_text) >= 0:
+            elif len(result_content) > 1 and matchFound( search_text_re, body_snippet ):
                 lgr.debug( f'\tprice snippet is most likely:' )
                 lgr.debug( f'\t\t> {body_snippet}' )
                 price_snippet = body_snippet
-
+        try:
+            price_snippet
+        except NameError:
+            lgr.debug( f'No price snippet found...' )
+            price_snippet = 'Price not found'
     aldi_finds[hash] = {'title': title, 'url': result_url, 'price': price_snippet}
     dbm.add_result( hash, title, result_url, price_snippet )
 
@@ -132,6 +136,7 @@ for k, v in aldi_finds.items():
     message = "New Ad Posted for a " + env['search_text'] + ":\n\n" + v['title'] + "\n\n" + v['url'] + "\n\nPrice: " + v['price']
     lgr.debug( f"{message}" )
     if env['sms_key']:
+        # lgr.debug( f"Not sending SMS for now..." )
         lgr.debug( f"Sending SMS..." )
         send_sms( env['sms_key'], message )
 
