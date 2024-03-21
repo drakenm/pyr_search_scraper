418a2570add9a3ae9d8cf50a4a502b2301b634ec
drakenm <drakenm@gmail.com>
2024-03-21 15:52:41 -0500

    move main method to util

--- app/app.py
+++ app/app.py
@@ -1,9 +1,8 @@
-from bs4 import BeautifulSoup as soup
-import requests, sys, re, hashlib, sqlite3
+import sys
 from lib._logger import Log_Init
 from lib._bootstrapper import Bootstrapper as Boot
 from os import environ as env
-from lib._database import Database_Manager
+
 from lib._utility import Utility
 
 try:
@@ -20,90 +19,18 @@ lgr.debug( '======App begin logging======' )
 lgr.debug( f'APP ROOT: {env["app_root"]}' )
 lgr.debug( f'APP ENTRY: {env["app_entry"]}' )
 
-url = f"{env['url_base']}{env['url_path']}{env['url_query']}"
-lgr.debug( f'URL: {url}' )
+if env['search_text_set_len'] == '1':
+    url = f"{env['url_base']}{env['url_path']}{env['url_query']}"
+    lgr.debug( f'URL: {url}' )
+else:
+    url_list = []
+    search_params = []
+    for i in range(0, int(env['search_text_set_len'])):
+        url_list.append( f"{env['url_base']}{env['url_path']}{env[f'url_query_{i}']}" )
+        search_params.append( env[f'search_text_{i}'] )
+    lgr.debug( f'URL List: {url_list}' )
 
-for u in url_list:
-    pass
-r = requests.get( url, headers={'User-agent': 'Mozilla/5.0'} )
-
-if r.status_code != 200:
-    lgr.error( f'Request failed with status code: {r.status_code}' )
-    sys.exit(1)
-
-lgr.debug( 'Request successful' )
-page = soup( r.content, 'html.parser' )
-search_result = page.find_all( attrs={"data-testid":"post-title-text"} )
-lgr.debug( f'Found {len(search_result)} search results' )
-
-dbm = Database_Manager( sqlite3.connect( env['app_db'] ) )
-
-aldi_finds:dict = {}
-
-for index, item in enumerate(search_result):
-    if not item:
-        lgr.debug( 'No title found' )
-        continue
-    title = item.get_text(strip=True, separator=" ").strip()
-    href = item['href']
-    filter_text_re = re.compile( r'' + env['filter_pattern'] )
-    search_text_re = re.compile( r'' + env['search_text'], re.IGNORECASE )
-
-    if not Utility.matchFound( filter_text_re, title ) or not Utility.matchFound( search_text_re, title ): continue
-
-    hash = Utility.get_hash( (title, href) )
-    # check if hash exists in db
-    if dbm.select_column( table='results', column='hash', data=hash ):
-        lgr.debug( f'{index}. Already exists in db...' )
-        lgr.debug( f'\t {hash}' )
-        lgr.debug( f'\t {title}' )
-        lgr.debug( f'\t {href}' )
-        continue
-
-    lgr.info(f'{index}. New result found!' )
-    lgr.info( f'\t {hash}' )
-    lgr.info( f'\t {title}' )
-    lgr.info( f'\t {href}' )
-
-    result_url = f"{env['url_base']}{href}"
-
-
-    lgr.debug( f'\tSearch Result Item URL: {result_url}' )
-    result_get = requests.get( result_url, headers={'User-agent': 'Mozilla/5.0'} )
-    result_page = soup( result_get.content, 'html.parser' )
-    price_text_re = re.compile( r'\$\d{1,6}' )
-    result_content = result_page.find_all( string=price_text_re )
-    
-    if len(result_content) > 0:
-        lgr.info( f'Found {len(result_content)} results containing a dollar sign ($)...' )
-        for index, item in enumerate(result_content):
-            body_snippet:str = item.get_text(strip=True, separator=" ").lower().strip()
-            if len(result_content) == 1:
-                lgr.debug( "\tOnly one result found...")
-                lgr.debug( f'\tprice snippet is most likely:' )
-                lgr.debug( f'\t\t> {body_snippet}' )
-                price_snippet = body_snippet
-            elif len(result_content) > 1 and Utility.matchFound( search_text_re, body_snippet ):
-                lgr.debug( f'\tprice snippet is most likely:' )
-                lgr.debug( f'\t\t> {body_snippet}' )
-                price_snippet = body_snippet
-        try:
-            price_snippet
-        except NameError:
-            lgr.debug( f'No price snippet found...' )
-            price_snippet = 'Price not found'
-    aldi_finds[hash] = {'title': title, 'url': result_url, 'price': price_snippet}
-    dbm.add_result( hash, title, result_url, price_snippet )
-
-for k, v in aldi_finds.items():
-    lgr.info( f'Found {v["title"]} at {v["url"]} for {v["price"]}' )
-    lgr.debug("building sms message...")
-    message = f"New Ad Posted for a {env['search_text']}:\n\n{v['title']}\n\n{v['url']}\n\nPrice: {v['price']}"
-    lgr.debug( f"{message}" )
-    if env['sms_key']:
-        # lgr.debug( f"Not sending SMS for now..." )
-        lgr.debug( f"Sending SMS..." )
-        Utility.send_sms( env['sms_key'], message )
-
+for p,u in zip(search_params, url_list):
+    Utility.do_the_thing(p, u, env['filter_pattern'])
 
 lgr.debug( '======App end logging======' )
--- app/lib/_utility.py
+++ app/lib/_utility.py
@@ -1,7 +1,9 @@
 from lib._logger import Log_Init
-import hashlib, re, requests
+from bs4 import BeautifulSoup as soup
+import hashlib, re, requests, sqlite3, sys
+from lib._database import Database_Manager
 from os import environ as env
-class Utility():
+class Utility:
     lgr = Log_Init( logger_name=env['app_log_name'], app_log_path=env['app_log'] ).getLogger()
 
     @staticmethod
@@ -39,3 +41,85 @@ class Utility():
             Utility.lgr.error( f'Failed to send SMS: {r.status_code}' )
             Utility.lgr.error( f'Response: {r.text}' )
         return r.text
+    
+    @staticmethod
+    def do_the_thing( search_text:str, url:str, filter_pattern:str ) -> None:
+        r = requests.get( url, headers={'User-agent': 'Mozilla/5.0'} )
+
+        if r.status_code != 200:
+            Utility.lgr.error( f'Request failed with status code: {r.status_code}' )
+            sys.exit(1)
+
+        Utility.lgr.debug( 'Request successful' )
+        page = soup( r.content, 'html.parser' )
+        search_result = page.find_all( attrs={"data-testid":"post-title-text"} )
+        Utility.lgr.debug( f'Found {len(search_result)} search results' )
+
+        dbm = Database_Manager( sqlite3.connect( env['app_db'] ) )
+
+        aldi_finds:dict = {}
+
+        for index, item in enumerate(search_result):
+            if not item:
+                Utility.lgr.debug( 'No title found' )
+                continue
+            title = item.get_text(strip=True, separator=" ").strip()
+            href = item['href']
+            filter_text_re = re.compile( r'' + env['filter_pattern'] )
+            search_text_re = re.compile( r'' + env['search_text'], re.IGNORECASE )
+
+            if not Utility.matchFound( filter_text_re, title ) or not Utility.matchFound( search_text_re, title ): continue
+
+            hash = Utility.get_hash( (title, href) )
+            # check if hash exists in db
+            if dbm.select_column( table='results', column='hash', data=hash ):
+                Utility.lgr.debug( f'{index}. Already exists in db...' )
+                Utility.lgr.debug( f'\t {hash}' )
+                Utility.lgr.debug( f'\t {title}' )
+                Utility.lgr.debug( f'\t {href}' )
+                continue
+
+            Utility.lgr.info(f'{index}. New result found!' )
+            Utility.lgr.info( f'\t {hash}' )
+            Utility.lgr.info( f'\t {title}' )
+            Utility.lgr.info( f'\t {href}' )
+
+            result_url = f"{env['url_base']}{href}"
+
+
+            Utility.lgr.debug( f'\tSearch Result Item URL: {result_url}' )
+            result_get = requests.get( result_url, headers={'User-agent': 'Mozilla/5.0'} )
+            result_page = soup( result_get.content, 'html.parser' )
+            price_text_re = re.compile( r'\$\d{1,6}' )
+            result_content = result_page.find_all( string=price_text_re )
+            
+            if len(result_content) > 0:
+                Utility.lgr.info( f'Found {len(result_content)} results containing a dollar sign ($)...' )
+                for index, item in enumerate(result_content):
+                    body_snippet:str = item.get_text(strip=True, separator=" ").lower().strip()
+                    if len(result_content) == 1:
+                        Utility.lgr.debug( "\tOnly one result found...")
+                        Utility.lgr.debug( f'\tprice snippet is most likely:' )
+                        Utility.lgr.debug( f'\t\t> {body_snippet}' )
+                        price_snippet = body_snippet
+                    elif len(result_content) > 1 and Utility.matchFound( search_text_re, body_snippet ):
+                        Utility.lgr.debug( f'\tprice snippet is most likely:' )
+                        Utility.lgr.debug( f'\t\t> {body_snippet}' )
+                        price_snippet = body_snippet
+                try:
+                    price_snippet
+                except NameError:
+                    Utility.lgr.debug( f'No price snippet found...' )
+                    price_snippet = 'Price not found'
+            aldi_finds[hash] = {'title': title, 'url': result_url, 'price': price_snippet}
+            dbm.add_result( hash, title, result_url, price_snippet )
+
+        for k, v in aldi_finds.items():
+            Utility.lgr.info( f'Found {v["title"]} at {v["url"]} for {v["price"]}' )
+            Utility.lgr.debug("building sms message...")
+            message = f"New Ad Posted for a {env['search_text']}:\n\n{v['title']}\n\n{v['url']}\n\nPrice: {v['price']}"
+            Utility.lgr.debug( f"{message}" )
+            if env['sms_key']:
+                # Utility.lgr.debug( f"Not sending SMS for now..." )
+                Utility.lgr.debug( f"Sending SMS..." )
+                Utility.send_sms( env['sms_key'], message )
