768b86800cd7d1af68f6c8f7997ae4b71b0c503a
drakenm <drakenm@gmail.com>
2024-03-21 16:35:13 -0500

    import util after bootstrap, all args as lists

--- app/app.py
+++ app/app.py
@@ -3,8 +3,6 @@ from lib._logger import Log_Init
 from lib._bootstrapper import Bootstrapper as Boot
 from os import environ as env
 
-from lib._utility import Utility
-
 try:
     Boot(sys.argv)
 except Exception as e:
@@ -13,6 +11,8 @@ except Exception as e:
     print( f'Exiting' )
     sys.exit(1)
 
+from lib._utility import Utility
+
 lgr = Log_Init( logger_name=env['app_log_name'], app_log_path=env['app_log'] ).getLogger()
 
 lgr.debug( '======App begin logging======' )
@@ -20,17 +20,19 @@ lgr.debug( f'APP ROOT: {env["app_root"]}' )
 lgr.debug( f'APP ENTRY: {env["app_entry"]}' )
 
 if env['search_text_set_len'] == '1':
-    url = f"{env['url_base']}{env['url_path']}{env['url_query']}"
-    lgr.debug( f'URL: {url}' )
+    url_list = [f"{env['url_base']}{env['url_path']}{env['url_query']}"]
+    search_params = [env['search_text']]
 else:
     url_list = []
     search_params = []
     for i in range(0, int(env['search_text_set_len'])):
         url_list.append( f"{env['url_base']}{env['url_path']}{env[f'url_query_{i}']}" )
         search_params.append( env[f'search_text_{i}'] )
-    lgr.debug( f'URL List: {url_list}' )
+lgr.debug( f'URL(s): {url_list}' )
 
 for p,u in zip(search_params, url_list):
-    Utility.do_the_thing(p, u, env['filter_pattern'])
+    # Utility.do_the_thing(p, u, env['filter_pattern'])
+    lgr.debug( f'Param: {p}' )
+    lgr.debug( f'URL: {u}' )
 
 lgr.debug( '======App end logging======' )
