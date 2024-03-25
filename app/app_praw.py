7494c15dc726559cde95618b5480793f11c8d75a
drakenm <drakenm@gmail.com>
2024-03-24 23:16:22 -0500

    move main to class

--- app/app_praw.py
+++ app/app_praw.py
@@ -1,4 +1,4 @@
-import sys, praw, re
+import sys, praw
 from lib._logger import Log_Init
 from lib._bootstrapper import Bootstrapper as Boot
 from os import environ as env
@@ -40,39 +40,6 @@ for p,u in zip(search_params, url_list):
     lgr.debug( f'Param: {p}' )
     lgr.debug( f'URL: {u}' )
     gafs = rdt.subreddit(env['subreddit'])
-    raw_search_param = p.strip('"')
-    filter_text_re = re.compile( env['filter_pattern'], re.IGNORECASE )
-    price_text_re = re.compile( r'\$\d{1,6}' )
-    new_line_re = re.compile( r'\n' )
-    search_text_re = re.compile( r'' + raw_search_param, re.IGNORECASE )
-    for i in gafs.search(p, sort='new', limit=5):
-        id = i.id
-        title = i.title
-        url = i.url
-        if Utility.matchFound( filter_text_re, i.title ) and Utility.matchFound( search_text_re, i.title ):
-            print(i.id, ":", i.title, ":", i.url)
-            keyword_index = i.selftext.find(raw_search_param)
-            left_newline = i.selftext.rfind( '\n', 0, keyword_index )
-            right_newline = i.selftext.find( '\n', keyword_index, len(i.selftext))
-            price_snippet = i.selftext[left_newline+1:right_newline]
-            print( f'keyword index: {keyword_index}' )
-            print( f'left newline: {left_newline}' )
-            print( f'right newline: {right_newline}' )
-            print( f'Price: {price_snippet}' )
-            # works well but what is a dollar sign doesn't exist in the derived price snippet?
-            if not Utility.matchFound( price_text_re, price_snippet ):
-                print( f'Price snippet does NOT contain a dollar sign...' )
-                print( f'Looking after the string for a dollar sign...' )
-                adjusted_keyword_index = i.selftext.find('$', right_newline, len(i.selftext))
-                adjusted_left_newline = i.selftext.rfind( '\n', 0, adjusted_keyword_index )
-                adjusted_right_newline = i.selftext.find( '\n', adjusted_keyword_index, len(i.selftext))
-                adjusted_price_snippet = i.selftext[left_newline+1:adjusted_right_newline]
-                print( f'adjusted keyword index: {adjusted_keyword_index}' )
-                print( f'adjusted left newline: {adjusted_left_newline}' )
-                print( f'adjusted right newline: {adjusted_right_newline}' )
-                print( f'adjusted price snippet: {adjusted_price_snippet}' )
-
-    aldi_finds[id] = {'title': title, 'url': url, 'price': price_snippet}
-    dbm.add_result( id, title, url, price_snippet )
+    Utility.do_the_thing( gafs, p, env['filter_pattern'], 20 )
 
 lgr.debug( '======App end logging======' )
